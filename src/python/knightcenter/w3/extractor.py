8958f7e63e16fed7cd42a6a356fbcb28259d7171
ig2gi <gilbert.perrin@gmail.com>
2012-11-16 15:37:37 +0100

    add overall-3 graph

--- src/python/knightcenter/w3/extractor.py
+++ src/python/knightcenter/w3/extractor.py
@@ -14,6 +14,21 @@ import math
 
 data_dir = '../../../nodebox3/knightcenter/w3/data/'
 
+class Score(object):
+  """
+  """
+  def __init__(self, name, s2011, s2012) :
+    self.name = name
+    self.s2011 = s2011.split('%')[0]
+    self.s2012 = s2012.split('%')[0]
+    
+  def csv(self):
+    return "%s,%s,%s" % (self.name,self.s2011,self.s2012)
+    
+  def delta(self):
+    return self.s2012 - self.s2011
+    
+
 
 def main():
   lines = [line.strip() for line in open('overall-result.csv')]   
@@ -41,15 +56,35 @@ def main():
     f.write(",".join(row) + '\n')  
   f.close()
   
+  
+  scores = []
+
+  
   lines = [line.strip() for line in open('Aid transparency - 2011 and 12 comparable data.csv')]
-  new_lines = []
-  for l in lines:
-    values = l.split(',')
-    new_lines.append([v.split('%')[0] for v in values[2:6]])
+  for l in lines[1:59]:
+    values = l.split(',')[2:6]
+    name = values[0]
+    s2011 = values[1].split('%')[0]
+    s2012 = values[2].split('%')[0]
+    scores.append(Score(name, s2011, s2012))
+    
   f = open(data_dir + "overall-result-3.csv", 'w+')
-  for l in new_lines:
-    f.write(",".join(l) + '\n')  
+  f.write('shortname,s2011,s2012\n')
+  for s in scores:
+    f.write(s.csv() + '\n')  
   f.close() 
+  
+  
+  # 
+  f = open(data_dir + "overall-result-3-2.csv", 'w+')
+  f.write('group,n2011,n2012\n')
+  for k in range(20,120,20):
+    group = "%i-%i" % (k-20, k)
+    n2011 = len([s for s in scores if float(s.s2011) >= (k - 20) and float(s.s2011) < k])
+    n2012 = len([s for s in scores if float(s.s2012) >= (k - 20) and float(s.s2012) < k])
+    f.write("%s,%i,%i" % (group,n2011,n2012) + '\n')  
+  f.close()
+  
   
 
     
